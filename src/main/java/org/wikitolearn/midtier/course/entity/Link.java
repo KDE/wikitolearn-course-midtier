3164b077b07b4d2745ad64e2fc3756643c065388
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-05-17 16:46:16 +0200

    Added links to EntityList object
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/main/java/org/wikitolearn/midtier/course/entity/Link.java
+++ src/main/java/org/wikitolearn/midtier/course/entity/Link.java
@@ -1,12 +1,8 @@
 package org.wikitolearn.midtier.course.entity;
 
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_DEFAULT)
-public class EntityList<T> {
-     @JsonProperty("_items")
-     List<T> items;
-     @JsonProperty("_meta")
-     Meta meta;
-     @JsonProperty("_links")
-     Map<String, Link> links;
+public class Link {
+  private String title;
+  private String href;
 }
