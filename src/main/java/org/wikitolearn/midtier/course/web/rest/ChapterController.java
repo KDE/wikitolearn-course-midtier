135eac4cd0dc5edc28412766daf18bbbc551da8e
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-07-05 23:47:36 +0200

    Added GET /chapters/{chapterId}
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/main/java/org/wikitolearn/midtier/course/web/rest/ChapterController.java
+++ src/main/java/org/wikitolearn/midtier/course/web/rest/ChapterController.java
@@ -1,12 +1,19 @@
 package org.wikitolearn.midtier.course.web.rest;
 
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +28,15 @@ import org.wikitolearn.midtier.course.entity.dto.in.AddChapterPagesDto;
 import org.wikitolearn.midtier.course.entity.dto.in.UpdateChapterDto;
 import org.wikitolearn.midtier.course.entity.dto.in.UpdateChapterPagesDto;
 import org.wikitolearn.midtier.course.entity.dto.out.AddedChapterPagesDto;
+import org.wikitolearn.midtier.course.entity.dto.out.GetChapterDto;
 import org.wikitolearn.midtier.course.entity.dto.out.UpdatedChapter;
 import org.wikitolearn.midtier.course.exception.InvalidResourceCreateException;
 import org.wikitolearn.midtier.course.service.ChapterService;
+import org.wikitolearn.midtier.course.service.PageService;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -40,12 +51,42 @@ public class ChapterController {
   private ChapterService chapterService;
   
   @Autowired
+  private PageService pageService;
+  
+  @Autowired
   private ModelMapper modelMapper;
+  
+  @Autowired
+  private ObjectMapper objectMapper;
+  
+  @ApiResponses({
+    @ApiResponse(code = 200, message = "Success", response = GetChapterDto.class),
+    @ApiResponse(code = 404, message = "Not Found", response = ErrorJson.class)
+  })
+  @GetMapping(value = "/{chapterId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public GetChapterDto getChapter(@PathVariable String chapterId) throws JsonProcessingException {
+
+    MultiValueMap<String, String> pagesParams = new LinkedMultiValueMap<>();
+    ObjectNode projectionJsonObject = objectMapper.getNodeFactory().objectNode().put("title", 1);
+    pagesParams.add("projection", objectMapper.writeValueAsString(projectionJsonObject));
+    
+    Chapter chapter = chapterService.find(chapterId, null);
+    chapter.setPages(
+        Optional
+        .ofNullable(chapter.getPages())
+        .orElseGet(Collections::emptyList)
+        .parallelStream()
+        .map(page -> pageService.find(page.getId(), pagesParams))
+        .collect(Collectors.toList())
+    );
+    return modelMapper.map(chapter, GetChapterDto.class);
+  }
   
   @ApiResponses({
     @ApiResponse(code = 200, message = "Success", response = UpdatedChapter.class),
     @ApiResponse(code = 401, message = "Unauthorized"),
     @ApiResponse(code = 403, message = "Forbidden"),
+    @ApiResponse(code = 404, message = "Not Found", response = ErrorJson.class),
     @ApiResponse(code = 422, message = "Uprocessable Entity"),
     @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorJson.class)
   })
